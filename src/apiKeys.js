8aac016bb0f2f1886d26fdfbe6d277a57f18a75a
Sayan Sarkar <95311933+sayansarkar2001@users.noreply.github.com>
2024-01-25 13:32:34 +0530

    Update apiKeys.js
    key commit

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "715fe9cfbdb433f90b7f8d72302a38be",
-  base: "https://api.openweathermap.org/data/2.5/",
+  // key: "715fe9cfbdb433f90b7f8d72302a38be",
+  // base: "https://api.openweathermap.org/data/2.5/",
 };
